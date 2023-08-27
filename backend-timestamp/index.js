b9f34f7548e6a3dba7a7580ed9a21f4f65b75441
Armin Pasic <arminpasic@Armins-MacBook-Pro.local>
2023-08-28 01:29:00 +0200

    Add header parser

--- backend-timestamp/index.js
+++ backend-timestamp/index.js
@@ -50,7 +50,7 @@ app.get('/api/:dateString?', (req, res) => {
     }
   }
 
-  if (date.toString === 'Invalid Date') {
+  if (date.toString() === 'Invalid Date') {
     res.json({ 'error': date.toString()});
   } else {
      res.json({ 'unix': date.getTime(), utc: date.toUTCString()});
